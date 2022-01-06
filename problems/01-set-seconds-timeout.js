3c07ab1605bb3496c10a1bfb30ed696edc214b03
Jared Kunhart <hakujaden@comcast.net>
2022-01-06 13:07:38 -0600

    first problem

--- problems/01-set-seconds-timeout.js
+++ problems/01-set-seconds-timeout.js
@@ -8,11 +8,15 @@ in *seconds*. The function should set a timeout on the given callback for
 the specified amount of time. Feel free to use the built-in `setTimeout`
 in your implementation.
 
-In addition to Mocha, we recommend that you test your code manually using 
-node with the examples below. Use the command 
+In addition to Mocha, we recommend that you test your code manually using
+node with the examples below. Use the command
 `node problems/01-set-seconds-timeout.js`
+***********************************************************************/
 
-Examples:
+function setSecondsTimeout(cb, delayInSeconds) {
+  let delay = delayInSeconds * 1000;
+  return setTimeout(cb, delay);
+}
 
 setSecondsTimeout(function () {
     console.log('hello');
@@ -21,11 +25,6 @@ setSecondsTimeout(function () {
 setSecondsTimeout(function () {
     console.log('world');
 }, 1.4); // should print 'world' after 1400 milliseconds
-***********************************************************************/
-
-function setSecondsTimeout(cb, delayInSeconds) {
-  // Your code here
-}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
